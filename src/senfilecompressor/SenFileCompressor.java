9a38e284f36adbf59cbd6fa2d1f06197df6c3629
Dieylany TALL <realkad41@gmail.com>
2018-02-09 22:12:16 +0000

    archivage de deux fichiers

--- src/senfilecompressor/SenFileCompressor.java
+++ src/senfilecompressor/SenFileCompressor.java
@@ -33,6 +33,7 @@ public class SenFileCompressor {
      */
     public static void main(String[] args) {
         Path src = Paths.get("mon-fichier.txt");
+        Path src2 = Paths.get("001-al-fatihah.mp3");
         Path encoded = Paths.get("encoded.temp");
         // Create the set of options for appending to the file.
         Set<OpenOption> options = new HashSet<OpenOption>();
@@ -40,26 +41,41 @@ public class SenFileCompressor {
         options.add(CREATE);
             
         try(
-            SeekableByteChannel sbc = Files.newByteChannel(src); 
-            SeekableByteChannel sbc2 = Files.newByteChannel(encoded, options);
+            SeekableByteChannel sbcSrc = Files.newByteChannel(src);
+            SeekableByteChannel sbcSrc2 = Files.newByteChannel(src2);
+            SeekableByteChannel sbcDst = Files.newByteChannel(encoded, options);
         ){
             ByteBuffer buf = ByteBuffer.allocate(10);
             long size = Files.size(src);
             String fileHeader = src.toFile().getName() + "," + Long.valueOf(size).toString()+"\n";
             byte[] temp = fileHeader.getBytes();
             ByteBuffer tempBuf = ByteBuffer.wrap(temp);
-            sbc2.write(tempBuf);
-            while(sbc.read(buf)>0){
+            sbcDst.write(tempBuf);
+            while(sbcSrc.read(buf)>0){
                 byte[] bytes = buf.array();
                 ByteBuffer bb = ByteBuffer.wrap(bytes);
-                sbc2.write(bb);
+                sbcDst.write(bb);
                 /*for(byte b: bytes){
                     System.out.print(String.format("%8s", Integer.toBinaryString(b & 0xFF)).replace(' ', '0'));
                 }
                 System.out.println();*/
                 buf.flip();
             }
-            
+            size = Files.size(src2);
+            fileHeader = src2.toFile().getName() + "," + Long.valueOf(size).toString()+"\n";
+            temp = fileHeader.getBytes();
+            tempBuf = ByteBuffer.wrap(temp);
+            sbcDst.write(tempBuf);
+            while(sbcSrc2.read(buf)>0){
+                byte[] bytes = buf.array();
+                ByteBuffer bb = ByteBuffer.wrap(bytes);
+                sbcDst.write(bb);
+                /*for(byte b: bytes){
+                    System.out.print(String.format("%8s", Integer.toBinaryString(b & 0xFF)).replace(' ', '0'));
+                }
+                System.out.println();*/
+                buf.flip();
+            }
         }catch(IOException e){
             System.out.println("Une exception: "+e);
         }
